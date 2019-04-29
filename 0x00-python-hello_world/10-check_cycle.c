de3f3ccd4e1bff316e1fd8b59d674dd8934a43c2
Drew <andrew.maring@gmail.com>
2019-04-29 23:43:48 +0000

    Add lists.h and 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for a loop/cycle in a singly linked list
+ * @list: singly linked list
+ *
+ * Return: 0 if no cyle found, 1 if a cycle is found
+ */
+int check_cycle(listint_t *list)
+{
+	while (list->next)
+	{
+		if (list < list->next)
+			return (1);
+
+		list = list->next;
+	}
+
+	return (0);
+}
