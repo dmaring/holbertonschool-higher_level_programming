7f2e71a43aaf7e8286284ec8ec3af525c42d9bb5
Drew <andrew.maring@gmail.com>
2019-05-01 02:18:19 +0000

    Actually save check for NULL pointer

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,7 +19,7 @@ listint_t *insert_node(listint_t **head, int number)
 	newnode->n = number;
 	newnode->next = NULL;
 
-	if (number < (*head)->n)
+	if (!(*head) || (number < (*head)->n))
 	{
 		temp = *head;
 		*head = newnode;
