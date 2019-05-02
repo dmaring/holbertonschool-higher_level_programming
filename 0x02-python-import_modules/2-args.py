bed606dcf1323888d7f072c74a08a5193b3f56d5
Drew <andrew.maring@gmail.com>
2019-05-02 20:41:36 +0000

    Add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+syslen = len(sys.argv)
+if syslen == 1:
+    print("0 arguments.")
+elif syslen == 2:
+    print("1 argument:")
+elif syslen > 2:
+    print("{} arguments:".format(len(sys.argv)))
+
+for num, element in enumerate(sys.argv):
+    if num > 0:
+        print("{}: {}".format(num, element))
