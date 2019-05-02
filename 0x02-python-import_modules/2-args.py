62cb1071728274ebc0ffb1bef4eb126bc52f9901
Drew <andrew.maring@gmail.com>
2019-05-02 20:45:34 +0000

    Add check for importing script into another module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-syslen = len(sys.argv)
-if syslen == 1:
-    print("0 arguments.")
-elif syslen == 2:
-    print("1 argument:")
-elif syslen > 2:
-    print("{} arguments:".format(len(sys.argv)))
+if __name__ == "__main__":
+    syslen = len(sys.argv)
+    if syslen == 1:
+        print("0 arguments.")
+    elif syslen == 2:
+        print("1 argument:")
+    elif syslen > 2:
+        print("{} arguments:".format(len(sys.argv)))
 
-for num, element in enumerate(sys.argv):
-    if num > 0:
-        print("{}: {}".format(num, element))
+    for num, element in enumerate(sys.argv):
+        if num > 0:
+            print("{}: {}".format(num, element))
