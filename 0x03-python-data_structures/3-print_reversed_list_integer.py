eba1e3b1e8fab425713a7e6e9df69139d20bf302
Drew <andrew.maring@gmail.com>
2019-05-07 21:27:49 +0000

    Add integer modifier to print format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,4 +5,4 @@ def print_reversed_list_integer(my_list=[]):
     """Print a list of integers in reverse"""
     my_list.reverse()
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
