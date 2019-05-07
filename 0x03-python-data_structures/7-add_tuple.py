06be33f5f86561a9dd5ffbe9f0db463d562d76cf
Drew <andrew.maring@gmail.com>
2019-05-07 23:30:46 +0000

    Fix for PEP8 and tuples greater than 2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,12 +3,12 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     """Returns a tuple where the first element should be the addition of the
-    first element of each argument. The second element should be the addition of
-    the second element of each argument
+    first element of each argument. The second element should be the addition
+    of the second element of each argument
     """
-    if len(tuple_a) == 2:
+    if len(tuple_a) >= 2:
         a1, a2 = tuple_a[0], tuple_a[1]
-    if len(tuple_b) == 2:
+    if len(tuple_b) >= 2:
         b1, b2 = tuple_b[0], tuple_b[1]
     if len(tuple_a) == 1:
         a1 = tuple_a[0]
