dde94f2a2800c310950852500822e48568f05653
Drew <andrew.maring@gmail.com>
2019-05-07 21:00:10 +0000

    Add 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    """Retrieve an element from a list like C"""
+    if idx < 0 or (len(my_list) - 1 < idx):
+        return (None)
+    return(my_list[idx])
