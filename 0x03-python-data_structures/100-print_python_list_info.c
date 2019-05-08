faab6d3f18eb3def14ae7b5b62e90c91ee8a09f1
Drew <andrew.maring@gmail.com>
2019-05-08 04:13:30 +0000

    Use PyListObject to cast object

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -12,13 +12,14 @@
 void print_python_list_info(PyObject *p)
 {
 	int size = PyList_Size(p);
+	PyListObject *pyobj = (PyListObject *)p;
 	int i = 0;
 
 	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", size);
+	printf("[*] Allocated = %li\n", pyobj->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Element %d: int\n", i);
+		printf("Element %d: %s\n", i, Py_TYPE(pyobj->ob_item[i])->tp_name);
 	}
 }
