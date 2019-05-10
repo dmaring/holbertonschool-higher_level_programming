e77899de32916b53ee9e2cfea5133577d6fd02dc
Drew <andrew.maring@gmail.com>
2019-05-10 01:26:51 +0000

    Use intersection operator for 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,9 +2,4 @@
 
 
 def common_elements(set_1, set_2):
-    myset = set()
-    for i in set_1:
-        for j in set_2:
-            if i is j:
-                myset.add(i)
-    return(myset)
+    return(set_1 & set_2)
