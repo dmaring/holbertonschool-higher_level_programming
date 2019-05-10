3e367de71fb4cb703a39d46a123b5b22a808df74
Drew <andrew.maring@gmail.com>
2019-05-10 02:43:31 +0000

    Change spacing around parameter options for pep8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,8 @@
 
 def best_score(a_dictionary):
     try:
-        mySortList = sorted(a_dictionary.items(), key = lambda kv: kv[1],
-                            reverse = True)
+        mySortList = sorted(a_dictionary.items(), key=lambda kv: kv[1],
+                            reverse=True)
         return(mySortList[0][0])
     except:
         return(None)
