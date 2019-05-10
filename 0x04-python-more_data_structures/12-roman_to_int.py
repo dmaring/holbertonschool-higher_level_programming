503f4ea075cdb931849ab05f6004d3953daf319f
Drew <andrew.maring@gmail.com>
2019-05-10 20:24:33 +0000

    Check that character in string is roman numeral

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,27 +3,26 @@
 
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
-        return(None)
+        return(0)
 
-    def get_int(single_rn):
-        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-                          'D': 500, 'M': 1000}
-        return(roman_numerals[single_rn])
 
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                      'D': 500, 'M': 1000}
     sum = 0
     i = 0
 
-    while (i < len(roman_string)):
-        first = get_int(roman_string[i])
+    while (i < len(roman_string) and roman_string[i] in roman_numerals):
+        first = roman_numerals[roman_string[i]]
 
         if (i + 1 < len(roman_string)):
-            second = get_int(roman_string[i + 1])
+            second = roman_numerals[roman_string[i + 1]]
             if first >= second:
                 sum += first
                 i += 1
             else:
                 sum += second - first
                 i += 2
+
         else:
             sum += first
             i += 1
