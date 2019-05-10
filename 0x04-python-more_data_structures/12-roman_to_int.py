babb78cadcb4d0d1819ae6549e76a0ebdcc4ff9e
Drew <andrew.maring@gmail.com>
2019-05-10 03:38:16 +0000

    Add 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if type(roman_string) != str or roman_string is None:
+        return(None)
+
+    def get_int(single_rn):
+        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                          'D': 500, 'M': 1000}
+        return(roman_numerals[single_rn])
+
+    sum = 0
+    i = 0
+
+    while (i < len(roman_string)):
+        first = get_int(roman_string[i])
+
+        if (i + 1 < len(roman_string)):
+            second = get_int(roman_string[i + 1])
+            if first < second:
+                sum += second - first
+                i += 2
+            else:
+                sum += first
+                i += 1
+        else:
+            sum += first
+            i += 1
+    return(sum)
