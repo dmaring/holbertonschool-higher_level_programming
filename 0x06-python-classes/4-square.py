78a7e90044dbd0c3fd36bacfbc5acd28946bd8b2
Drew <andrew.maring@gmail.com>
2019-05-22 00:22:02 +0000

    Fix 4-square.py for pep8

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,6 +12,7 @@ class Square:
     def size(self):
         """Get the size of the square"""
         return self.__size
+
     @size.setter
     def size(self, value):
         """Set the size of the square"""
