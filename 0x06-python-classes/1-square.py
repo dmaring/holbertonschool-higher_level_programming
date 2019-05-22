fec41eb19e04c00343babb7285753460ef99a6ff
Drew <andrew.maring@gmail.com>
2019-05-22 00:23:15 +0000

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class Square():
+    """Class that defines a square"""
+
+    def __init__(self, size):
+        """Initiate an instance of the Square class"""
+        self.__size = size
