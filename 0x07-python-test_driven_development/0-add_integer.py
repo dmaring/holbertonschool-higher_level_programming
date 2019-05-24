323cd9f7990638e2ade894c322a01cc578a1c0c9
Drew <andrew.maring@gmail.com>
2019-05-24 15:50:48 +0000

    Add README.md tests folder and 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+"""
+Integers addition module
+"""
+
+def add_integer(a, b=98):
+    """This function returns the sum of two integers"""
+    if (type(a) is not int) and (type(a) is not float):
+        raise TypeError("a must be an integer")
+    if (type(b) is not int) and (type(b) is not float):
+        raise TypeError("a must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return(a + b)
