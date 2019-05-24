5fa6a25146bab3b090b24b38b60cb468ef64f75e
Drew <andrew.maring@gmail.com>
2019-05-24 19:00:18 +0000

    Add 3-say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+"""
+Divide a matrix module
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that divides all elements of a matrix and ret a new matrix"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name == "":
+        print("My name is {}".format(first_name, last_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
