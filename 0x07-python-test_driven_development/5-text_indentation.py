17d652660b382b9879e1b36554cdbb63fcc9b453
Drew <andrew.maring@gmail.com>
2019-05-24 23:13:15 +0000

    Add 5-text_indentation files

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+"""
+A function that a function that prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    A function that a function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    """
+
+    if not isinstance(text, type("")):
+        raise TypeError("text must be a string")
+
+    line = ""
+    i = 0
+    while i < len(text):
+        if any([text[i] is ".", text[i] is "?", text[i] is ":"]):
+            line += text[i]
+            line = line.strip()
+            print(line, end='')
+            print()
+            print()
+            line = ''
+            i += 1
+        else:
+            line += text[i]
+            i += 1
+    if line != '':
+        print(line.strip(), end='')
