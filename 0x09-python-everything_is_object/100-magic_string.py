479fa1650c85d0e6e0b4b673b88fdf50728be3e7
Drew <andrew.maring@gmail.com>
2019-05-30 23:34:36 +0000

    Add 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    nonlocal num = 0
-    num += 1
-    return(["magic_string" for x in range(num)])
+def magic_string(n = []):
+    n.append("Holberton")
+    return(", ".join(n))
