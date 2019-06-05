22d556c24eadeba8a50be4ef0aead9d830b80e84
Drew <andrew.maring@gmail.com>
2019-06-05 21:16:37 +0000

    Add files 1 to 3

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+A module that contains the lookup function.
+"""
+
+
 def lookup(obj):
+    """
+    returns the attributes and methods contained in an object
+    """
+
     return(dir(obj))
