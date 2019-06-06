842f11f7bd44f71bee8348e62704b55b91f60e1c
Drew <andrew.maring@gmail.com>
2019-06-06 00:44:27 +0000

    Edit 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ Module to demonstrate inheritance
 def inherits_from(obj, a_class):
     """Check if the obj is a subclass of a_class"""
 
-    if (issubclass(type(obj), a_class)):
+    if (isinstance(obj, a_class)) and (type(obj) != a_class):
         return(True)
     else:
         return(False)
