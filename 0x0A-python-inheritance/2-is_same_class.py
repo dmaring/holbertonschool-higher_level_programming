2e61dfa1adce9f06f133f2e144a70bd948ef491c
Drew <andrew.maring@gmail.com>
2019-06-06 00:57:46 +0000

    Modify files for pep8

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Module that contains is_same_class function"""
+
+
 def is_same_class(obj, a_class):
+    """returns True if the object is
+    exactly an instance of the specified class
+     ; otherwise False."""
+
     if type(obj) is a_class:
         return(True)
     else:
