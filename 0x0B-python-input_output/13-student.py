5d23dab6d7941c61752a8e2118d060137f1d058f
Drew <andrew.maring@gmail.com>
2019-06-07 04:39:16 +0000

    Add 13-student.py

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""student class module"""
+
+
+class Student:
+    """
+    Class that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and hasattr(self, '__dict__'):
+            ret_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__ and type(attr) is str:
+                    ret_dict[attr] = self.__dict__[attr]
+            return(ret_dict)
+        else:
+            return(self.__dict__.copy())
+
+    def reload_from_json(self, json):
+        self.__init__(json['first_name'], json['last_name'], json['age'])
