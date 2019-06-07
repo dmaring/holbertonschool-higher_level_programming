7e4d1c11caa63721d198db7567cf261fc882c5fd
Drew <andrew.maring@gmail.com>
2019-06-07 03:51:32 +0000

    Add execute permission to 10-class_to_json.py

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -6,7 +6,7 @@ import json
 def class_to_json(obj):
     """
     A function that returns the dictionary description with
-    simple data structure (list, dictionary, string, integer
+    simple data structure (list, dict, string, integer
     and boolean) for JSON serialization of an object.
     """
 
