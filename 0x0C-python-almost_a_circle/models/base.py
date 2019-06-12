7c8221100992b47b78e8c134568867c1941bd9f9
Drew <andrew.maring@gmail.com>
2019-06-12 22:18:01 +0000

    Add class method create() and corresponding tests for Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import json
 """
 This module contains the Base class
 """
+import json
+
 
 
 class Base():
@@ -14,6 +15,12 @@ class Base():
         """Method to initiate an object instance of Base"""
         self.id = id
 
+    @staticmethod
+    def reset_id():
+        """Method that resets Base private attribute __nb_objects to 0"""
+        Base.__nb_objects = 0
+
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Method that takes a list of dictionaries and returns
@@ -23,12 +30,52 @@ class Base():
         if not list_dictionaries or list_dictionaries == None:
             return([])
         else:
-            ret_str = json.dumps(list_dictionaries)
+            ret_str = json.dumps(list_dictionaries, sort_keys=True)
         return(ret_str)
 
+
     @staticmethod
-    def reset_id():
-        Base.__nb_objects = 0
+    def from_json_string(json_string):
+        """
+        Method that returns a list of the JSON string representation
+        json_string: string representing a list of dictionaries
+        """
+        if not json_string or json_string == None or json_string == '[]':
+            return([])
+        else:
+            return(json.loads(json_string))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Method that saves a list of dictionary objects
+        as json file
+        """
+
+        data = []
+        if list_objs is None:
+            fpn = 'None.json'
+            data = '[]'
+        else:
+            fpn = '{}.json'.format(list_objs[0].__class__.__name__)
+            # need to create a list of dictionaries from
+            # the list of object instances
+            for obj in list_objs:
+                obj_dict = obj.to_dictionary()
+                data.append(obj_dict)
+        json_data = cls.to_json_string(data)
+        with open(fpn, 'w', encoding="utf-8") as fp:
+            fp.write(json_data)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Method that creates an instance from a dictionary"""
+        # print(cls)
+        # print("Dir of cls is: {}".format(dir(cls)))
+        # print("__dict__ of cls is {}".format(cls.__dict__))
+        r1 = cls(1, 2, 3, 4)
+        r1.update(**dictionary)
+        return(r1)
 
     @property
     def id(self):
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 """
 Module for unittests for the Base class
 """
@@ -43,19 +45,149 @@ class TestBaseClassCreation(unittest.TestCase):
         bo = Base(float('NaN'))
         #TODO what should this do
 
-    # def test_nb_objects(self):
-    #     bo = Base(23)
-    #     self.assertEqual(Base.__nb_objects, 0)
-# def test_isupper(self):
-  #     self.assertTrue('FOO'.isupper())
-  #     self.assertFalse('Foo'.isupper())
 
-  # def test_split(self):
-  #     s = 'hello world'
-  #     self.assertEqual(s.split(), ['hello', 'world'])
-  #     # check that s.split fails when the separator is not a string
-  #     with self.assertRaises(TypeError):
-  #         s.split(2)
+class TestBaseToJsonStringMethod(unittest.TestCase):
+    """Test class for Base class to_json_string method"""
+
+    #test passing None
+    def test_pass_none(self):
+        ret = Base.to_json_string(None)
+        self.assertEqual(ret, [])
+
+    def test_pass_empty_list(self):
+        ret = Base.to_json_string([])
+        self.assertEqual(ret, [])
+
+    def test_pass_one(self):
+        Base.reset_id()
+        r1 = Rectangle(3, 4)
+        rd = r1.to_dictionary()
+        str_ = '{"height": 4, "id": 1, "width": 3, "x": 0, "y": 0}'
+        self.assertEqual(Base.to_json_string(rd), str_)
+
+    def test_pass_list_dicts(self):
+        Base.reset_id()
+        r1 = Rectangle(3, 4, 5, 6)
+        rd1 = r1.to_dictionary()
+        r2 = Rectangle(8, 9, 10, 11)
+        rd2 = r2.to_dictionary()
+        ret = Base.to_json_string([rd1, rd2])
+        str_ = '[{"height": 4, "id": 1, "width": 3, "x": 5, "y": 6}, {"height": 9, "id": 2, "width": 8, "x": 10, "y": 11}]'
+        self.assertEqual(ret, str_)
+
+    def test_pass_list_dicts_type(self):
+        Base.reset_id()
+        r1 = Rectangle(3, 4, 5, 6)
+        rd1 = r1.to_dictionary()
+        r2 = Rectangle(8, 9, 10, 11)
+        rd2 = r2.to_dictionary()
+        ret = Base.to_json_string([rd1, rd2])
+        self.assertIsInstance(ret, str)
+
+
+
+class TestBaseToSaveToFileMethod(unittest.TestCase):
+    """Test class for Base class method save_to_file"""
+
+    def test_correct_file_save_rectangle(self):
+        Base.reset_id()
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Base.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as file:
+            file_read = file.read()
+        cmp_str = '[{"height": 7, "id": 1, "width": 10, "x": 2, "y": 8}, {"height": 4, "id": 2, "width": 2, "x": 0, "y": 0}]'
+        self.assertEqual(file_read, cmp_str)
+
+    def test_correct_file_save_square(self):
+        Base.reset_id()
+        s1 = Square(10, 4, 2)
+        s2 = Square(2, 3, 2)
+        Base.save_to_file([s1, s2])
+        with open("Square.json", "r") as file:
+            file_read = file.read()
+        cmp_str = '[{"height": 10, "id": 1, "width": 10, "x": 4, "y": 2}, {"height": 2, "id": 2, "width": 2, "x": 3, "y": 2}]'
+        self.assertEqual(file_read, cmp_str)
+
+    def test_write_empty_file(self):
+        Base.reset_id()
+        Base.save_to_file(None)
+        with open("None.json", "r") as file:
+            file_read = file.read()
+        self.assertEqual(file_read, '"[]"')
+
+class TestBaseFromJsonStringMethod(unittest.TestCase):
+    """Test class for Base class method json_to_string_method"""
+    def setUp(self):
+        Base.reset_id()
+        self.single_item = '[{"height": 4, "id": 1, "width": 3, "x": 0, "y": 0}]'
+        self.mult_item = '[{"height": 4, "id": 1, "width": 3, "x": 0, "y": 0}, {"height": 6, "id": 2, "width": 5, "x": 0, "y": 0}]'
+
+    #test passing empty list
+    def test_passing_empyt_list(self):
+        self.assertEqual(Base.from_json_string('[]'), [])
+
+    #test passing None instead of list
+    def test_passing_none(self):
+        self.assertEqual(Base.from_json_string(None), [])
+
+    #test that object returned is correct with multiple dictionaries
+    def test_pass_multiple(self):
+        ret_val = Base.from_json_string(self.mult_item)
+        cor_val = [{'id': 1, 'y': 0, 'height': 4, 'x': 0, 'width': 3}, {'id': 2, 'y': 0, 'height': 6, 'x': 0, 'width': 5}]
+        self.assertEqual(ret_val, cor_val)
+
+    #test that object returned is correct with 1 dictionary
+    def test_pass_single(self):
+        ret_val = Base.from_json_string(self.single_item)
+        cor_val = [{'id': 1, 'y': 0, 'height': 4, 'x': 0, 'width': 3}]
+        self.assertEqual(ret_val, cor_val)
+
+    #test object returned is list with 1 dictionary passed
+    def test_pass_single_type_check(self):
+        ret_val = Base.from_json_string(self.single_item)
+        self.assertIsInstance(ret_val, list)
+
+    #test object returned is a list with multiple dictionaries
+    def test_pass_mult_type_check(self):
+        ret_val = Base.from_json_string(self.mult_item)
+        self.assertIsInstance(ret_val, list)
+
+class TestBaseDictionaryToInstance(unittest.TestCase):
+    """Test class for Base class method dictionary_to_instance"""
+    def setUp(self):
+        Base.reset_id()
+        self.r1 = Rectangle(3, 5, 1)
+        self.r1_dictionary = self.r1.to_dictionary()
+        self.r2 = Rectangle.create(**self.r1_dictionary)
+
+        self.s1 = Square(3, 5, 1)
+        self.s1_dictionary = self.s1.to_dictionary()
+        self.s2 = Square.create(**self.s1_dictionary)
+
+    def test_create_rectangle(self):
+        self.assertEqual(str(self.r1), str(self.r2))
+
+    def test_create_square(self):
+        self.assertEqual(str(self.s1), str(self.s2))
+
+    def test_instance_rectangle(self):
+        self.assertIsInstance(self.r1, Rectangle)
+
+    def test_instance_square(self):
+        self.assertIsInstance(self.s1, Square)
+
+    def test_not_is_rectangle(self):
+        self.assertIsNot(self.r1, self.r2)
+
+    def test_not_is_square(self):
+        self.assertIsNot(self.s1, self.s2)
+
+    def test_is_not_equal_rectangle(self):
+        self.assertNotEqual(self.r1, self.r2)
+
+    def test_is_not_equal_square(self):
+        self.assertNotEqual(self.s1, self.s2)
 
 if __name__ == '__main__':
     unittest.main()
