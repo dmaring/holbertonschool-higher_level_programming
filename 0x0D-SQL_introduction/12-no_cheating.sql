393d4acec99bb05bd695b8c15c1e6819087f4412
Drew <andrew.maring@gmail.com>
2019-07-15 22:35:24 +0000

    Add

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- a script that updates the score of Bob to 10 in the table second_table
+-- use Bob's name value to select and update his score
+UPDATE `second_table`
+SET
+`score` = 10
+WHERE `name` = 'Bob'
