9f78985dcd2227ae7735e0045391116d4f0cde81
Drew <andrew.maring@gmail.com>
2019-07-15 21:19:28 +0000

    Add 4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- a script that creates a table called first_table in the current database
+-- creates the table named first_table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS `first_table` (id INT,
+name VARCHAR(256));
