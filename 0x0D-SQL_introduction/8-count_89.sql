3084d5eb352cf3409508c359a6855b97c5551f42
Drew <andrew.maring@gmail.com>
2019-07-15 22:01:44 +0000

    Update 8-count_89.sql for syntax

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- a script that displays the number of records with id = 89
 -- a SQL query that returns the count of records where id = 89
-SELECT COUNT(*) from `first_table` WHERE `id` = 89
+SELECT COUNT(*) FROM `first_table` WHERE `id` = 89
