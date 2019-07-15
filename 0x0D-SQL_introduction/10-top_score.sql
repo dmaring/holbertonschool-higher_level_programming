1bbd5e31941ca703cef041773f7e9035e222ecf4
Drew <andrew.maring@gmail.com>
2019-07-15 22:21:54 +0000

    Add 10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- a script that lists all records of the table second_table
+-- query returns records ordered by score (top first)
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
