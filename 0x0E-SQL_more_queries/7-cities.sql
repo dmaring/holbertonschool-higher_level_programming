9aee177879bf000a4237bff28a5f7c55f38eb509
Drew <andrew.maring@gmail.com>
2019-07-16 21:28:17 +0000

    Add

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- SQL statement that creates hbtn_0d_usa database
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- SQL statement that creates the table states
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (id INT UNIQUE NOT NULL AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY (id),
