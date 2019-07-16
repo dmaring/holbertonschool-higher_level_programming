74035fb24232db26eda4043ef0678e7e1bb471b5
Drew <andrew.maring@gmail.com>
2019-07-16 00:13:38 +0000

    Add correct field name for 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,8 +1,8 @@
 -- a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
 -- SQL query that retrieves city temperature averages
-SELECT `state`, MAX(`value`) as `avg_temp`
+SELECT `state`, MAX(`value`) as `max_temp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `state`
-ORDER BY `avg_temp` DESC
+ORDER BY `max_temp` DESC
 LIMIT 3;
