3df0aa8e42da18e348c4d53b34faf1f49756e234
Drew <andrew.maring@gmail.com>
2019-07-16 20:29:08 +0000

    Add 3-force_name.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- a script that creates the table force_name
+-- creates the table force_name with id and name attributes
+CREATE TABLE IF NOT EXISTS `force_name` (id INT, name VARCHAR(256) NOT NULL);
