4906c5bbf0431b39a46eb41c11069fb66312357e
Drew <andrew.maring@gmail.com>
2019-07-16 20:32:08 +0000

    Add

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- a script that creates the table id_not_null
+-- creates the table id_not_null with id and name attributes
+CREATE TABLE IF NOT EXISTS `id_not_null` (id INT NOT NULL DEFAULT 1, name VARCHAR(256));
