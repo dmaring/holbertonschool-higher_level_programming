b9353a5860111a4a1a9cce1bb453cd383c59d721
Drew <andrew.maring@gmail.com>
2019-07-16 19:20:57 +0000

    Add 0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- a MYSQL statement that shows the privileges for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- a MYSQL statement that shows the privileges for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
