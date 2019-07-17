9a7e2b7289d37eebdab75c818d600c8cd77bb44d
Drew <andrew.maring@gmail.com>
2019-07-17 00:39:51 +0000

    Add condition where genre_id IS NOT NULL

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,5 @@
 SELECT tv_shows.title as title, tv_show_genres.genre_id as genre_id
 FROM tv_shows INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id IS NOT NULL
 ORDER BY title, genre_id ASC;
