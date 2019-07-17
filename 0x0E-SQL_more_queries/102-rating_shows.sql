e1c0907914ca04d681bf90df9029df7b54bcee36
Drew <andrew.maring@gmail.com>
2019-07-17 03:09:37 +0000

    Add 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- a script that lists all shows from hbtn_0d_tvshows_rate by their rating
 -- SQL query that shows tv shows by their rating
-SELECT title, SUM(rate) as rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) as rating
 FROM tv_shows
 JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
