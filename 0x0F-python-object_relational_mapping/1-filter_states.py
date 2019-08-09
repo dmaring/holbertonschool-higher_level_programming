e24d4ca1fab709c94ee3ba890cf04716a75e4545
Drew <andrew.maring@gmail.com>
2019-08-09 19:38:29 +0000

    Add 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+# a script that lists all states with a name starting
+# with N (upper N) from the database hbtn_0e_0_usa
+import MySQLdb
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+dbname = sys.argv[3]
+conn = MySQLdb.connect(host="localhost",
+                       port=3306,
+                       user=username,
+                       passwd=password,
+                       db=dbname,
+                       charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE " +
+            "SUBSTRING(name, 1, 1) = 'N' ORDER BY id ASC;")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
