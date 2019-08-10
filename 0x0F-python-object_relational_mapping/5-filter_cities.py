44e3214821560f76160296f215800ce2ab3b65f1
Drew <andrew.maring@gmail.com>
2019-08-10 23:16:01 +0000

    Add sql ORDER BY to 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     cur.execute("SELECT cities.name " +
                 "FROM cities JOIN states " +
                 "ON cities.state_id = states.id " +
-                "WHERE BINARY states.name = %s;", (statename,))
+                "WHERE states.name = %s ORDER BY cities.id ASC;", (statename,))
     query_rows = cur.fetchall()
     for i, row in enumerate(query_rows):
         row = row[0]
