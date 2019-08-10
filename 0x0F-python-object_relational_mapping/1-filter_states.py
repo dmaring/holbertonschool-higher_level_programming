1612a183e3c181be4bb9fb1ee8c51db2126954b5
Drew <andrew.maring@gmail.com>
2019-08-10 22:51:55 +0000

    Use SQL LIKE in 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                            charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE " +
-                "SUBSTRING(name, 1, 1) = 'N' ORDER BY id ASC;")
+                "name LIKE 'N%' ORDER BY id ASC;")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
