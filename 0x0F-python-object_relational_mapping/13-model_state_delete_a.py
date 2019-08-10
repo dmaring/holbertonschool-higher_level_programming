d1ca6a7f62fe85c74f39646f40a9c3e1db7c20d0
Drew <andrew.maring@gmail.com>
2019-08-10 00:48:44 +0000

    Add 13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+a script that changes the name of a State object from the database
+hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password,
+                                   dbname), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    session.query(State).filter(
+        State.name.like('%a%')).delete(synchronize_session=False)
+    session.commit()
+    session.close()
