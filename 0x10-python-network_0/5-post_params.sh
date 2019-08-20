69b12c9eb2c8f554bbb72344691dc7843ad3fcf6
Drew <andrew.maring@gmail.com>
2019-08-20 22:08:04 +0000

    Edit 5-post_params.sh for correct body message

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that sends a POST request
-curl -s -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be there for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
