91ef76d55877507ac6102f68633ccffc2130d24f
Drew <andrew.maring@gmail.com>
2019-08-26 19:25:02 +0000

    Use decode method on res content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,15 +9,12 @@ def getStatus():
     """
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
-        c_type = type(content)
+        c_type = type(content.decode())
         headers = res.headers
-        _utf8 = ""
-        if 'charset=utf-8' in headers.get('Content-Type'):
-            _utf8 = "OK"
         print("Body response:")
         print("\t- type: {}".format(c_type))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(_utf8))
+        print("\t- utf8 content: {}".format(content.decode('utf-8'))
 
 if __name__ == '__main__':
     getStatus()
