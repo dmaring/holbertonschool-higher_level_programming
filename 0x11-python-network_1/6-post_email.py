f0dd4d969c767623247d4cf9cea57568f58ae5f4
Drew <andrew.maring@gmail.com>
2019-08-26 18:58:40 +0000

    Add 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""A script sends an email"""
+import requests
+import sys
+
+
+def sendEmail():
+    """
+    A function that sends POST and prints the response
+    """
+    _data = {'email': sys.argv[2]}
+    _url = sys.argv[1]
+    res = requests.request('POST', _url, data=_data)
+    print(res.text)
+
+if __name__ == '__main__':
+    sendEmail()
