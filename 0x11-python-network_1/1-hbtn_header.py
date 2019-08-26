b6d0da33a4cfd9d133793b5bdada48848dd911f2
Drew <andrew.maring@gmail.com>
2019-08-26 17:09:09 +0000

    Add 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""A script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+import sys
+
+
+def getStatus():
+    """
+    A function that fetches https://intranet.hbtn.io/status
+    """
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        content = res.read()
+        c_type = type(content)
+        headers = res.headers
+        print(headers['X-Request-Id'])
+
+
+if __name__ == '__main__':
+    getStatus()
