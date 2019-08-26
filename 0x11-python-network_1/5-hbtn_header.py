06a23a70951bdcdea2b0fbed802ae27a709b1828
Drew <andrew.maring@gmail.com>
2019-08-26 18:26:46 +0000

    Add

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+A script that fetches a header value for X-Request-Id
+"""
+import requests
+import sys
+
+
+def getStatus():
+    """
+    A function that fetches a header value for X-Request-Id
+    """
+    res = requests.get('https://intranet.hbtn.io/status')
+    _id = res.headers.get('X-Request-Id')
+    print(_id)
+
+
+if __name__ == '__main__':
+    getStatus()
