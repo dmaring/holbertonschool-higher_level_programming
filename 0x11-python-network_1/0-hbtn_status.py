79d3351f23a62b9ea609fbe34434a42a727b5e94
Drew <andrew.maring@gmail.com>
2019-08-26 16:58:37 +0000

    Add 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""A script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+def getStatus():
+    """
+    A function that fetches https://intranet.hbtn.io/status
+    """
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        c_type = type(content)
+        headers = res.headers
+        _utf8 = ""
+        if 'charset=utf-8' in headers['Content-Type']:
+            _utf8 = "OK"
+        print("Body response:")
+        print("\t- type: {}".format(c_type))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(_utf8))
+
+if __name__ == '__main__':
+    getStatus()
