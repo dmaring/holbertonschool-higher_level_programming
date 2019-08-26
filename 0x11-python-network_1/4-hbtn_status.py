c5bb640f2cca7ea2cf63cc1a250243577b276eeb
Drew <andrew.maring@gmail.com>
2019-08-26 18:21:16 +0000

    Add 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+A script that fetches https://intranet.hbtn.io/status
+with request module
+"""
+import requests
+import sys
+
+
+def getStatus():
+    """
+    A function that fetches https://intranet.hbtn.io/status
+    with the requests module
+    """
+    res = requests.get('https://intranet.hbtn.io/status')
+    print(res.status_code)
+
+
+if __name__ == '__main__':
+    getStatus()
