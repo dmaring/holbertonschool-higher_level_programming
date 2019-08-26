59c805bc5efeea39a49788f24f8fe14bf19148a6
Drew <andrew.maring@gmail.com>
2019-08-26 19:09:27 +0000

    Add 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""A script that fetches the http response header code"""
+import requests
+import sys
+
+
+def getStatus():
+    """
+    A function that fetches the http response header code
+    """
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
+
+if __name__ == '__main__':
+    getStatus()
