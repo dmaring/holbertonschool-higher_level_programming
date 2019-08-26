4551085b26ff6db673d757012f7db6ab172ff532
Drew <andrew.maring@gmail.com>
2019-08-26 21:53:06 +0000

    Add 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+A Python script that uses the Github API to return the userid
+"""
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+def searchAPI():
+    """
+    A function that uses the Github API to return the userid
+    """
+    res = requests.get('http://api.github.com/user',
+                       auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    _dict = res.json()
+    print(_dict.get('id'))
+
+
+if __name__ == '__main__':
+    searchAPI()
