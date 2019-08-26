b9f939a10527078d6f9f90dc733c318a64942e84
Drew <andrew.maring@gmail.com>
2019-08-26 18:10:22 +0000

    Add 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""A script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+import urllib.error
+import sys
+
+
+def getStatus():
+    """
+    A function that fetches https://intranet.hbtn.io/status
+    """
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as res:
+            content = res.read()
+            print(content.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == '__main__':
+    getStatus()
