9e5c83fe16a4c723bc23f03ddf7fede1b4278bbe
Drew <andrew.maring@gmail.com>
2019-08-26 21:06:57 +0000

    Add 9-starwars.py

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+A Python script that searches the star wars api
+"""
+import requests
+import sys
+
+
+def searchAPI():
+    """
+    A function that searches the star wars api
+    """
+    _params = {'search': sys.argv[1]}
+    _url = 'https://swapi.co/api/people/'
+    res = requests.request('GET', _url, params=_params)
+    _dict = res.json()
+    print("Number of results: {}".format(_dict.get('count')))
+    for result in _dict.get('results'):
+        print(result.get('name'))
+
+
+if __name__ == '__main__':
+    searchAPI()
