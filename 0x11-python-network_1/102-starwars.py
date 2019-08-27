7cef502d9bf163346330a835577ecd169454c821
Drew <andrew.maring@gmail.com>
2019-08-27 00:30:25 +0000

    Add 102-starwars.py

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+A Python script that searches the star wars api
+"""
+import requests
+import sys
+
+
+def movieName(url):
+    """
+    a function that returns the name of a star wars movie
+    """
+
+    res = requests.get(url)
+    title = res.json().get('title')
+    return('\t {}'.format(title))
+
+
+def searchAPI():
+    """
+    A function that searches the star wars api
+    """
+    _params = {'search': sys.argv[1]}
+    _url = 'https://swapi.co/api/people/'
+    res = requests.request('GET', _url, params=_params)
+    _dict = res.json()
+    print("Number of results: {}".format(_dict.get('count')))
+    for result in _dict.get('results'):
+        print(result.get('name'))
+        for film in result.get('films'):
+            print(movieName(film))
+    while _dict.get('next'):
+        _url = _dict['next']
+        res = requests.request('GET', _url)
+        _dict = res.json()
+        for result in _dict.get('results'):
+            print(result.get('name'))
+            for film in result.get('films'):
+                print(movieName(film))
+
+
+if __name__ == '__main__':
+    searchAPI()
