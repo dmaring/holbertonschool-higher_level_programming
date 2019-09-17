2bb98ae7d9622cd6909b60340f73115df67af5b5
Drew Maring <andrew.maring@gmail.com>
2019-09-17 11:56:06 -0700

    Add 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const x = parseInt(process.argv[2])
+if (isNaN(x)) {
+  console.log(1)
+}
+
+function factorial(a) {
+  if (a === 0) {
+    return 1
+  } else {
+    return a * (factorial(a - 1))
+  }
+}
+
+console.log(factorial(x))
