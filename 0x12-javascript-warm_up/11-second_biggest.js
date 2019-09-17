4086a73007314ff9bbaf34c31661a6d787aed2b2
Drew Maring <andrew.maring@gmail.com>
2019-09-17 13:38:18 -0700

    Add correct output for only 1 arg passed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const len = process.argv.length;
 if (len < 3) {
   console.log(0);
 } else if (len === 3) {
-  console.log(1);
+  console.log(0);
 } else {
   const args = process.argv.slice(2);
   args.sort();
