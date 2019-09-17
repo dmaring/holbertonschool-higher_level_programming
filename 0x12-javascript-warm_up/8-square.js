989f4914da18bb954b38f0a7263fda29bc7984d9
Drew Maring <andrew.maring@gmail.com>
2019-09-17 11:44:06 -0700

    Add 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const x = parseInt(process.argv[2])
+if (isNaN(x)) {
+  console.log('Missing size')
+} else if (x === 1) {
+  console.log('X')
+} else {
+  let row = 'X'
+  for (let t = 0; t < x - 1; t++) {
+    row = row + 'X'
+  }
+  for (let i = 0; i < x; i++) {
+    console.log(row)
+  }
+}
