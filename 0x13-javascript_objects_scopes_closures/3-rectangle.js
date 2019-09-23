cc2ece9b74a71241138f40cdf8f8ddafc3c911ff
Drew Maring <andrew.maring@gmail.com>
2019-09-23 14:11:41 -0700

    Add 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+      this.print = function () {
+        let row = '';
+        for (let t = 0; t < this.width; t++) {
+          row = row + 'X';
+        }
+        for (let i = 0; i < this.height; i++) {
+          console.log(row);
+        }
+      };
+    }
+  }
+}
+
+module.exports = Rectangle;
