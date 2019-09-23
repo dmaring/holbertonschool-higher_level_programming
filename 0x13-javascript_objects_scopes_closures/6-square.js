1d51fb82fb7af86932f8bef4dd6f83170ddd46f7
Drew Maring <andrew.maring@gmail.com>
2019-09-23 14:37:24 -0700

    Add 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const parSquare = require('./5-square');
+
+class Square extends parSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (letter) {
+    if (letter === undefined) {
+      letter = 'X';
+    }
+    let row = '';
+    for (let t = 0; t < this.width; t++) {
+      row = row + letter;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
