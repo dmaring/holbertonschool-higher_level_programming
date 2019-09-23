7f4fb9ede7cb4e2d0328d975bd717c99ed0fdeec
Drew Maring <andrew.maring@gmail.com>
2019-09-23 14:53:28 -0700

    Add 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const retArray = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    retArray.push(list[i]);
+  }
+  return (retArray);
+};
