e97367dbd725420a898c755632b1b2620f47ce8a
Drew Maring <andrew.maring@gmail.com>
2019-09-24 10:59:53 -0700

    Add 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+const outputFile = process.argv[2];
+const outputString = process.argv[3];
+
+fs.writeFile(outputFile, outputString, 'utf8', err => {
+  if (err) {
+    console.log(err);
+  }
+});
