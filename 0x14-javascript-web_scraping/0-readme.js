58407916bd6e92902a58f1771fe31d679fcf491d
Drew Maring <andrew.maring@gmail.com>
2019-09-24 10:50:17 -0700

    Add 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+const inputFile = process.argv[2];
+
+fs.readFile(inputFile, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
