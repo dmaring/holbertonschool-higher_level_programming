e3fc8d69398ea6a0be2de41dcdb121e41e4d73ba
Drew Maring <andrew.maring@gmail.com>
2019-09-24 11:24:27 -0700

    Add 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+const episode = process.argv[2];
+
+request('http://swapi.co/api/films/' + episode, (e, r, body) => {
+  console.log(JSON.parse(body).title);
+});
