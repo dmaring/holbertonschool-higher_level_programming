1efb3f0f6edee3b763a34015dbcd6eb1a268839f
Drew Maring <andrew.maring@gmail.com>
2019-09-24 12:19:22 -0700

    Add 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+function countCompleted (taskList) {
+  const retObj = {};
+  for (let i = 0; i < taskList.length; i++) {
+    const userId = taskList[i].userId;
+    const completed = taskList[i].completed;
+    if (Object.prototype.hasOwnProperty.call(retObj, userId) && completed) {
+      retObj[userId] += 1;
+    } else if (completed) {
+      retObj[userId] = 1;
+    }
+  }
+  console.log(retObj);
+}
+
+request
+  .get(url, (e, r, body) => {
+    countCompleted(JSON.parse(body));
+  });
