5238a4e3a8c659bb0594663442158f23b2c43d45
Drew Maring <andrew.maring@gmail.com>
2019-09-24 11:49:42 -0700

    Add 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+
+function countAppearances (movieList) {
+  for (let i = 0; i < movieList.length; i++) {
+    for (let j = 0; j < movieList[i].characters.length; j++) {
+      if (movieList[i].characters[j] === 'https://swapi.co/api/people/18/') {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+}
+
+request(url, (e, r, body) => {
+  countAppearances(JSON.parse(body).results);
+});
