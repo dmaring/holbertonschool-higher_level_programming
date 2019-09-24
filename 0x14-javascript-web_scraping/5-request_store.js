bf5826dc5a27e9803041747d008438964dd30bbc
Drew Maring <andrew.maring@gmail.com>
2019-09-24 12:00:30 -0700

    Add 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const outputFile = process.argv[3];
+
+function saveFile (content, outputFile) {
+  fs.writeFile(outputFile, content, 'utf8', (err) => {
+    if (err) throw err;
+  });
+}
+
+request
+  .get(url, 'utf8', (e, r, body) => {
+    saveFile(body, outputFile);
+  });
