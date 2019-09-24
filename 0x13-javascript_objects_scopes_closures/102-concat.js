209da70cd4197455fe9cc0f7c844aff7736c653a
Drew Maring <andrew.maring@gmail.com>
2019-09-23 18:50:44 -0700

    Add 102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const fs = require('fs');
+const textA = fs.readFileSync(fileA, 'utf8');
+const textB = fs.readFileSync(fileB, 'utf8');
+
+const newText = textA + '\n' + textB + '\n';
+
+fs.appendFile(fileC, newText, function (err) {
+  if (err) throw err;
+});
