7886652087badbe9a1125f59d34245e8c50d9441
Drew Maring <andrew.maring@gmail.com>
2019-09-24 14:15:43 -0700

    Add 100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/bin/usr/node
+const request = require('request');
+const movieId = process.argv[2];
+
+const getCharNames = function (charIdList) {
+  for (let i = 0; i < charIdList.length; i++) {
+    request.get(charIdList[i], (e, r, body) => {
+      console.log(JSON.parse(body).name);
+    });
+  }
+};
+
+request
+  .get('http://swapi.co/api/films/' + movieId, 'utf8', (e, r, body) => {
+    getCharNames(JSON.parse(body).characters);
+  });
