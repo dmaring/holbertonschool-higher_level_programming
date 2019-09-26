c29894e7a74e874f561fac27faea5dcc9c6ff275
Drew Maring <andrew.maring@gmail.com>
2019-09-26 14:31:49 -0700

    Add 9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+(function () {
+  $.ajax({
+    url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+    success: function (data) {
+      $('div#hello').text(data.hello);
+    }
+  });
+})();
