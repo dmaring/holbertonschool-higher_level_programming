df7793188060c7ed01aeba4b5ac3b63df7024118
Drew Maring <andrew.maring@gmail.com>
2019-09-26 13:56:57 -0700

    Add 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+(function () {
+  $.ajax({
+    url: 'https://swapi.co/api/people/5/?format=json',
+    success: function (data, status) {
+      $('div#character').text(data.name);
+    }
+  });
+})();
