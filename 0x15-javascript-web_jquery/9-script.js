dbec26e70a60b87d777058ebaafc9e29a987c632
Drew Maring <andrew.maring@gmail.com>
2019-09-26 14:33:53 -0700

    Make script invoke after DOM is ready

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,8 +1,8 @@
-(function () {
+$(function () {
   $.ajax({
     url: 'https://fourtonfish.com/hellosalut/?lang=fr',
     success: function (data) {
       $('div#hello').text(data.hello);
     }
   });
-})();
+});
